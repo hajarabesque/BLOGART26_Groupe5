73cd978b346b432e67c6a47d8473476443426559
djusteiiiiii <djuste.kabamba-mulowayi@mmibordeaux.com>
2026-02-06 09:18:03 +0100

    Add files via upload

--- api/comments/delete.php
+++ api/comments/delete.php
@@ -1,2 +1,15 @@
 <?php
+require_once '../../config.php';
+require_once '../../functions/ctrlSaisies.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $numCom = ctrlSaisies($_POST['numCom']);
+
+    // Soft delete: mark as logically deleted instead of actually deleting
+    sql_update("COMMENT", "delLogiq = 1", "numCom = $numCom");
+
+    header('Location: ../../views/backend/comments/list.php');
+    exit();
+}
+?>
 
